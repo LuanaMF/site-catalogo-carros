ec0dfe7dfd09473bb3fc69af81a4187abb53ac51
Luana Fraga <108876874+LuanMFraga@users.noreply.github.com>
2023-06-16 14:39:50 -0300

    next ui

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { CssBaseline } from '@nextui-org/react';
+import Button from '@nextui-org/react/button';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: React.Children.toArray([initialProps.styles])
+    };
+  }
+
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>{CssBaseline.flush()}</Head>
+        <body>
+          <Main />
+          <NextScript />
+          <Button>Click me</Button>
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
